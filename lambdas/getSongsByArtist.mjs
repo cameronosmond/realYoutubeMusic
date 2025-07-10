25b118509a3c1ad5f455fd2929060fb7467d9dd2
cameronosmond <cameronosmond05@gmail.com>
2025-07-10 12:08:47 -0400

    lay structure of getSongsByArtist

--- lambdas/getSongsByArtist.mjs
+++ lambdas/getSongsByArtist.mjs
@@ -1,8 +1,26 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { GetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+const ddb = DynamoDBDocumentClient.from(client);
+
 export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    try {
+        if (!event.body) {
+            return {
+              statusCode: 400,
+              body: JSON.stringify({ error: "Missing body" }),
+            };
+          }
+          
+          const body = JSON.parse(event.body);
+          const { artistName, userId } = body;
+
+    } catch (error) {
+        console.error("Error: ", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message || "Internal server error" }),
+        };
+    }
   };
