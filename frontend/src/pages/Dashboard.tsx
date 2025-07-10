95412793b33c2295e8c125a626d31e4b1e256158
cameronosmond <cameronosmond05@gmail.com>
2025-07-10 11:52:38 -0400

    add lambda call, add loading state

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FadeContent from "../components/FadeContent";
+import { FourSquare } from "react-loading-indicators";
 
 interface Props {
   userId: string;
@@ -6,21 +9,57 @@ interface Props {
 
 function Dashboard({ userId }: Props) {
   const [loading, setLoading] = useState(false);
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const navigate = useNavigate();
 
   const getSongs = async (formData: FormData) => {
     setLoading(true);
 
     const query = formData.get("artistName") as string;
     const artistName = query.toLowerCase();
+
+    // calling getSongsByArtist lambda function
+    const res = await fetch(`${apiUrl}/getSongsByArtist`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        artistName: artistName,
+        userId: userId,
+      }),
+    });
+
+    const data = await res.json();
+    setLoading(false);
+
+    if (res.ok) {
+      console.log("Songs: ", data.results);
+    } else {
+      console.error("Error:", data.error || data);
+    }
   };
 
   return (
-    <form action={getSongs}>
-      <label>
-        Enter artist name: <input name="artistName" />
-        <button type="submit">Search</button>
-      </label>
-    </form>
+    <FadeContent
+      blur={true}
+      duration={1000}
+      easing="ease-out"
+      initialOpacity={0}>
+      {loading ? (
+        <FourSquare
+          color="rgba(255, 255, 255, 0.87)"
+          size="large"
+          text="Loading"
+          textColor="rgba(255, 255, 255, 0.87)"
+        />
+      ) : (
+        <form action={getSongs}>
+          <label>
+            Enter artist name: <input name="artistName" />
+            <button type="submit">Search</button>
+          </label>
+        </form>
+      )}
+    </FadeContent>
   );
 }
 
