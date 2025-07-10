77caf03f8a091e6d668eb34153794243dd918807
cameronosmond <cameronosmond05@gmail.com>
2025-07-09 21:18:14 -0400

    add loading state into login, use ternary for if loading is true

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import FadeContent from "../components/FadeContent";
 import Lanyard from "../components/Landyard";
 
@@ -6,29 +7,40 @@ function Login() {
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   const apiUrl = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const client = window.google.accounts.oauth2.initCodeClient({
     client_id: clientId,
     scope: "openid https://www.googleapis.com/auth/youtube.readonly", // openid to get unique id_token for storing per user in db
     ux_mode: "popup",
     callback: (res: { code: string }) => {
+      setLoading(true);
+
       // Sending code to backend Lambda
-      fetch(`${apiUrl}/googleSignIn`, { // api gateway endpoint
+      fetch(`${apiUrl}/googleSignIn`, {
+        // api gateway endpoint
         method: "POST",
         body: JSON.stringify({ code: res.code }),
         headers: {
           "Content-Type": "application/json",
           "X-Requested-With": "XMLHttpRequest",
         },
-      }).then(async (res) => {
-        const data = await res.json();
-        if (res.ok) {
-          console.log("Success:", data.message);
-          navigate("/dashboard");
-        } else {
-          console.error("Error:", data.error || data);
-        }
-      });
+      })
+        .then(async (res) => {
+          const data = await res.json();
+          setLoading(false);
+
+          if (res.ok) {
+            console.log("Success:", data.message);
+            navigate("/dashboard");
+          } else {
+            console.error("Error:", data.error || data);
+          }
+        })
+        .catch((err) => {
+          setLoading(false);
+          console.error("Fetch error:", err);
+        });
     },
   });
 
@@ -38,12 +50,18 @@ function Login() {
       duration={1000}
       easing="ease-out"
       initialOpacity={0}>
-      <h1>Welcome to Youtube Music Playlist Manager</h1>
-      <Lanyard
-        position={[0, 0, 20]}
-        gravity={[0, -40, 0]}
-        onClick={() => client.requestCode()}
-      />
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <h1>Welcome to Youtube Music Playlist Manager</h1>
+          <Lanyard
+            position={[0, 0, 20]}
+            gravity={[0, -40, 0]}
+            onClick={() => client.requestCode()}
+          />
+        </>
+      )}
     </FadeContent>
   );
 }
