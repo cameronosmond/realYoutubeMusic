715e412e12403a4a5bc555c9038a6e3e651fb9c1
cameronosmond <cameronosmond05@gmail.com>
2025-07-10 11:44:28 -0400

    make outline of Dashboard structure

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 interface Props {
   userId: string;
 }
 
 function Dashboard({ userId }: Props) {
-  return <p></p>;
+  const [loading, setLoading] = useState(false);
+
+  const getSongs = async (formData: FormData) => {
+    setLoading(true);
+
+    const query = formData.get("artistName") as string;
+    const artistName = query.toLowerCase();
+  };
+
+  return (
+    <form action={getSongs}>
+      <label>
+        Enter artist name: <input name="artistName" />
+        <button type="submit">Search</button>
+      </label>
+    </form>
+  );
 }
 
 export default Dashboard;
