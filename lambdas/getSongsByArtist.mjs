c94fbca53b748fff7e15d03a71b49d10f3de63eb
cameronosmond <cameronosmond05@gmail.com>
2025-07-10 12:14:42 -0400

    get refresh_token from db

--- lambdas/getSongsByArtist.mjs
+++ lambdas/getSongsByArtist.mjs
@@ -11,17 +11,30 @@ export const handler = async (event) => {
               statusCode: 400,
               body: JSON.stringify({ error: "Missing body" }),
             };
-          }
+        }
           
-          const body = JSON.parse(event.body);
-          const { artistName, userId } = body;
+        const body = JSON.parse(event.body);
+        const { artistName, userId } = body;
 
-          if (event.headers['x-requested-with'] !== 'XMLHttpRequest') {
+        if (event.headers['x-requested-with'] !== 'XMLHttpRequest') {
             return {
               statusCode: 400,
               body: JSON.stringify('Invalid request'),
             }
-          }
+        }
+
+        // get refresh token associated with userId from db
+        const getResult = await ddb.send(
+            new GetCommand({
+                TableName: "ytMusicAppUsers",
+                Key: { user_id: userId },
+            })
+        );
+
+        const refreshToken = getResult.Item?.refresh_token;
+        if (!refreshToken) throw new Error("No refresh token found");
+
+        
 
     } catch (error) {
         console.error("Error: ", error);
