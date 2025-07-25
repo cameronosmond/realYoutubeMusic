7b47e34b94184275792e186576ea3335818a047b
cameronosmond <cameronosmond05@gmail.com>
2025-07-25 14:41:14 -0400

    update date on privacy policy

--- frontend/src/pages/Privacy.tsx
+++ frontend/src/pages/Privacy.tsx
@@ -2,7 +2,7 @@ export default function Privacy() {
   return (
     <div className="p-4 max-w-2xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
-      <p>Last updated: June 23, 2025</p>
+      <p>Last updated: July 25, 2025</p>
 
       <h2 className="mt-4 text-xl font-semibold">Information We Collect</h2>
       <p>
